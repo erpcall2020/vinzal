5cb1f7bd03722107dbec0dd8514af06286dc4d3b
“Odoo <erpcall@gmil.com>
2024-01-11 07:35:29 +0530

    requiredremove in visitoe

--- visitors_register/models/visitors_register.py
+++ visitors_register/models/visitors_register.py
@@ -8,9 +8,9 @@ class VisitorsRegister(models.Model):
     _name = 'visitors.register'
 
     name = fields.Char(string='Name', required=True, help="Name of the visitors")
-    phone = fields.Char(string='Phone', required=True)
-    purpose = fields.Text(string='Purpose' , required=True)
+    phone = fields.Char(string='Phone')
+    purpose = fields.Text(string='Purpose')
     address = fields.Text(string='Address')
-    check_out_time = fields.Datetime(string='Check Out Time', required=True)
+    check_out_time = fields.Datetime(string='Check Out Time')
 
 
