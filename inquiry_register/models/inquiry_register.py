46d6db3595342790425aec79edd7635e2935b4ea
“Odoo <erpcall@gmil.com>
2024-01-22 00:25:00 +0530

    inquiry

--- inquiry_register/models/inquiry_register.py
+++ inquiry_register/models/inquiry_register.py
@@ -6,7 +6,7 @@ class InquiryRegister(models.Model):
 
     name = fields.Char(string='Parent Name')
     student_name = fields.Char(string='Student Name')
-    child_age = fields.Integer(string='Child Age')
+    child_age = fields.Float(string='Child Age')
     phone_number = fields.Char(string='Phone Number')
     email = fields.Char(string='Email')
     purpose = fields.Text(string='Purpose')
